e8064fd086c6e7769df95c862e7807c46fd8979b
naveenraj duraisamy <naveenrj04@gmail.com>
2023-03-23 02:01:59 -0500

    search filter script

---
+++ searchfilter/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import JSONDATA from './MOCK_DATA.json';
+import {useState} from 'react';
+
+
+
+function App() {
+  const [searchterm, setSearchterm] = useState("");
+
+  const handleChange = (event) => {
+
+    setSearchterm(event.target.value);
+     
+  }
+
+  return (
+    <div className="App">
+      <h1>Search filter</h1>
+      <input type = "text" placeholder = "Search..." onChange={handleChange}/>
+     
+      {JSONDATA.filter((val)=> {
+          
+        if(searchterm === "")
+        {
+          return val
+        } else if(val.first_name.toLowerCase().includes(searchterm.toLowerCase()))
+        {
+          return val
+        }
+        
+
+      }).map((val, key) =>{
+        return(
+          <div className= "user" key ={key}>
+            <p>{val.first_name}</p>
+          </div>
+        )
+
+      })}
+      
+    </div>
+  );
+}
+
+export default App;
